d57a2908895d3db82e615be4768c7c6769e42ffa
hikhtiarta27 <54619914+hikhtiarta27@users.noreply.github.com>
2020-07-22 20:58:57 +0700

    db

--- aquarium.sql
+++ aquarium.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Waktu pembuatan: 20 Jul 2020 pada 18.12
+-- Waktu pembuatan: 22 Jul 2020 pada 15.58
 -- Versi server: 10.4.8-MariaDB
 -- Versi PHP: 7.3.10
 
@@ -39,9 +39,8 @@ CREATE TABLE `banner` (
 --
 
 INSERT INTO `banner` (`id`, `img`, `url`) VALUES
-(1, '1_20190926_192940.jpg', 'https://www.facebook.com/100031370685222/videos/155437378845284/'),
-(2, '2_gambar-pada-standing-banner-warna.webp', 'http://localhost/aquarium/admin/page_home'),
-(3, '3_og-bannersnack_v2.png', 'http://localhost/aquarium/admin/page_home');
+(9, 'c2411d46-b05d-4111-b313-f34292375090.jpg', 'http://localhost/aquarium/admin/page_home'),
+(10, '79e4ad5c-f3dc-42ba-b0ac-873e7f12dffa.jpg', 'http://localhost/aquarium/admin/page_about');
 
 -- --------------------------------------------------------
 
@@ -61,7 +60,7 @@ CREATE TABLE `category` (
 INSERT INTO `category` (`id`, `name`) VALUES
 (1, 'Kabinet'),
 (2, 'Lampu'),
-(6, 'Hasan Ikhtiarta');
+(8, 'Pasir');
 
 -- --------------------------------------------------------
 
@@ -77,15 +76,16 @@ CREATE TABLE `post` (
   `likes` int(11) NOT NULL DEFAULT 0,
   `share` int(11) NOT NULL DEFAULT 0,
   `created_date` timestamp NOT NULL DEFAULT current_timestamp(),
-  `updated_date` timestamp NULL DEFAULT NULL
+  `updated_date` timestamp NULL DEFAULT NULL,
+  `url` text NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
 -- Dumping data untuk tabel `post`
 --
 
-INSERT INTO `post` (`id`, `title`, `content`, `thumbnail`, `likes`, `share`, `created_date`, `updated_date`) VALUES
-(17, 'Cara Membuat Aquascape Murah dan Mudah ', 'Cara membuat aquascape', '17_thumbnail1on1.png', 0, 0, '2020-07-20 13:44:00', '2020-07-20 16:09:18');
+INSERT INTO `post` (`id`, `title`, `content`, `thumbnail`, `likes`, `share`, `created_date`, `updated_date`, `url`) VALUES
+(31, 'Map with Marker', '<p>1</p>', '[\"2020\\/07\\/21\\/59032ec5-d597-4875-adfd-8ca5f48aaa45.png\",\"2020\\/07\\/21\\/4d3b5f42-e5a6-44c7-9b78-ece77a56be75.png\"]', 0, 0, '2020-07-21 05:05:30', '2020-07-21 07:50:58', 'map-with-marker');
 
 -- --------------------------------------------------------
 
@@ -103,16 +103,19 @@ CREATE TABLE `product` (
   `created_date` timestamp NOT NULL DEFAULT current_timestamp(),
   `updated_date` timestamp NULL DEFAULT NULL,
   `likes` int(11) NOT NULL DEFAULT 0,
-  `share` int(11) NOT NULL DEFAULT 0
+  `share` int(11) NOT NULL DEFAULT 0,
+  `url` text NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
 -- Dumping data untuk tabel `product`
 --
 
-INSERT INTO `product` (`id`, `name`, `category`, `description`, `price`, `img`, `created_date`, `updated_date`, `likes`, `share`) VALUES
-(29, 'Pasir Putih kasar untuk dasar aquarium aquascape', '[\"Kabinet\",\"Lampu\",\"Hasan Ikhtiarta\",\"cek\"]', '<p>1</p>', 3123123, '1f101f5b-98c6-4d9f-9f05-ebb50f95e4c0.png', '2020-07-20 15:34:38', '2020-07-20 16:08:17', 0, 0),
-(30, 'Pasir Silika kasar untuk dasar aquarium aquascape', '[\"Hasan Ikhtiarta\"]', 'Pasir Silika kasar untuk dasar aquarium aquascape', 15000, '393e28ed-372d-48f9-bbf4-a445b943cb1a.png', '2020-07-20 15:55:14', '2020-07-20 16:08:01', 0, 0);
+INSERT INTO `product` (`id`, `name`, `category`, `description`, `price`, `img`, `created_date`, `updated_date`, `likes`, `share`, `url`) VALUES
+(38, 'Hiasan Aquascape 1:1', '[\"Kabinet\"]', '<p><span style=\"font-family: &quot;Open Sans&quot;, Arial, sans-serif; text-align: justify;\">Sudah merupakan fakta bahwa seorang pembaca akan terpengaruh oleh isi tulisan dari sebuah halaman saat ia melihat tata letaknya. Maksud penggunaan Lorem Ipsum adalah karena ia kurang lebih memiliki penyebaran huruf yang normal, ketimbang menggunakan kalimat seperti \"Bagian isi disini, bagian isi disini\", sehingga ia seolah menjadi naskah Inggris yang bisa dibaca. Banyak paket Desktop Publishing dan editor situs web yang kini menggunakan Lorem Ipsum sebagai contoh teks. Karenanya pencarian terhadap kalimat \"Lorem Ipsum\" akan berujung pada banyak situs web yang masih dalam tahap pengembangan. Berbagai versi juga telah berubah dari tahun ke tahun, kadang karena tidak sengaja, kadang karena disengaja (misalnya karena dimasukkan unsur humor atau semacamnya)</span><br></p>', 100000, '[\"2020\\/07\\/22\\/c52d757a-4b8f-4d03-80d6-1ff321187b45.jpg\",\"2020\\/07\\/22\\/7d16bbea-13eb-4f2f-ba13-b1c30e7839fd.jpg\",\"2020\\/07\\/22\\/080d1d77-4c19-4640-b239-b88a657870bf.jpg\"]', '2020-07-21 23:26:12', NULL, 0, 0, 'hiasan-aquascape-11'),
+(39, 'Aquascape Kayu 1:1', '[\"Kabinet\"]', '<p><span style=\"font-family: &quot;Open Sans&quot;, Arial, sans-serif; text-align: justify;\">Sudah merupakan fakta bahwa seorang pembaca akan terpengaruh oleh isi tulisan dari sebuah halaman saat ia melihat tata letaknya. Maksud penggunaan Lorem Ipsum adalah karena ia kurang lebih memiliki penyebaran huruf yang normal, ketimbang menggunakan kalimat seperti \"Bagian isi disini, bagian isi disini\", sehingga ia seolah menjadi naskah Inggris yang bisa dibaca. Banyak paket Desktop Publishing dan editor situs web yang kini menggunakan Lorem Ipsum sebagai contoh teks. Karenanya pencarian terhadap kalimat \"Lorem Ipsum\" akan berujung pada banyak situs web yang masih dalam tahap pengembangan. Berbagai versi juga telah berubah dari tahun ke tahun, kadang karena tidak sengaja, kadang karena disengaja (misalnya karena dimasukkan unsur humor atau semacamnya)</span><br></p>', 25000000, '[\"2020\\/07\\/22\\/252f211d-c222-4e74-aed5-d7f902f5a341.jpg\",\"2020\\/07\\/22\\/f3c3e59e-fb26-438a-bc65-93485b226107.jpg\",\"2020\\/07\\/22\\/afe63268-a0c3-43bd-9bf1-20b3651539d7.jpg\"]', '2020-07-21 23:26:44', NULL, 0, 0, 'aquascape-kayu-11'),
+(40, 'Batu Hias 1:1', '[\"Kabinet\"]', '<p><span style=\"font-family: &quot;Open Sans&quot;, Arial, sans-serif; text-align: justify;\">Sudah merupakan fakta bahwa seorang pembaca akan terpengaruh oleh isi tulisan dari sebuah halaman saat ia melihat tata letaknya. Maksud penggunaan Lorem Ipsum adalah karena ia kurang lebih memiliki penyebaran huruf yang normal, ketimbang menggunakan kalimat seperti \"Bagian isi disini, bagian isi disini\", sehingga ia seolah menjadi naskah Inggris yang bisa dibaca. Banyak paket Desktop Publishing dan editor situs web yang kini menggunakan Lorem Ipsum sebagai contoh teks. Karenanya pencarian terhadap kalimat \"Lorem Ipsum\" akan berujung pada banyak situs web yang masih dalam tahap pengembangan. Berbagai versi juga telah berubah dari tahun ke tahun, kadang karena tidak sengaja, kadang karena disengaja (misalnya karena dimasukkan unsur humor atau semacamnya)</span><br></p>', 100000000, '[\"2020\\/07\\/22\\/287f1d99-a47f-4688-82c9-38416e0b6428.jpg\",\"2020\\/07\\/22\\/db1020cd-c758-4eb0-9935-ea628615b945.jpg\",\"2020\\/07\\/22\\/3c12f246-2e22-4579-960d-46b1b206568a.jpg\"]', '2020-07-21 23:27:12', NULL, 0, 0, 'batu-hias-11'),
+(41, 'Lampu Hias Aquarium Besar Sedang Kecil 100x100', '[\"Lampu\"]', '<p><span style=\"font-family: &quot;Open Sans&quot;, Arial, sans-serif; text-align: justify;\">Sudah merupakan fakta bahwa seorang pembaca akan terpengaruh oleh isi tulisan dari sebuah halaman saat ia melihat tata letaknya. Maksud penggunaan Lorem Ipsum adalah karena ia kurang lebih memiliki penyebaran huruf yang normal, ketimbang menggunakan kalimat seperti \"Bagian isi disini, bagian isi disini\", sehingga ia seolah menjadi naskah Inggris yang bisa dibaca. Banyak paket Desktop Publishing dan editor situs web yang kini menggunakan Lorem Ipsum sebagai contoh teks. Karenanya pencarian terhadap kalimat \"Lorem Ipsum\" akan berujung pada banyak situs web yang masih dalam tahap pengembangan. Berbagai versi juga telah berubah dari tahun ke tahun, kadang karena tidak sengaja, kadang karena disengaja (misalnya karena dimasukkan unsur humor atau semacamnya)</span><br></p>', 80000000, '[\"2020\\/07\\/22\\/4f48ca50-85c1-4882-9eb5-6ae4785eaaed.jpg\",\"2020\\/07\\/22\\/cb4c65d6-fea5-4a2c-ad0a-6952252485bf.jpg\",\"2020\\/07\\/22\\/4f39548c-41ca-4a0f-9d6c-4822a3c426e3.jpg\"]', '2020-07-21 23:28:34', NULL, 0, 0, 'lampu-hias-aquarium-besar');
 
 -- --------------------------------------------------------
 
@@ -138,7 +141,7 @@ CREATE TABLE `user` (
 --
 
 INSERT INTO `user` (`id`, `username`, `password`, `role`, `name`, `toko_name`, `email`, `phone`, `address`, `bio`) VALUES
-('1', 'admin', '5994471abb01112afcc18159f6cc74b4f511b99806da59b3caf5a9c173cacfc5', 'admin', 'aquawabisabi', 'aquawabisabi', 'aquawabisabi@gmail.com', '081297991631', 'Wisma Staco 7th floor, Jl. Casablanca, RT.4/RW.12, Menteng Dalam, Tebet, South Jakarta City, Jakarta 12960', '<b>Toko ikan dsa das</b>');
+('1', 'admin', '5994471abb01112afcc18159f6cc74b4f511b99806da59b3caf5a9c173cacfc5', 'admin', 'aquawabisabi', 'aquawabisabi', 'hasan.ikhtiarta@gmail.com', '081297991631', 'Wisma Staco 7th floor, Jl. Casablanca, RT.4/RW.12, Menteng Dalam, Tebet, South Jakarta City, Jakarta 12960', '<b>Toko ikan dsa das</b>');
 
 --
 -- Indexes for dumped tables
@@ -182,25 +185,25 @@ ALTER TABLE `user`
 -- AUTO_INCREMENT untuk tabel `banner`
 --
 ALTER TABLE `banner`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=12;
 
 --
 -- AUTO_INCREMENT untuk tabel `category`
 --
 ALTER TABLE `category`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
 
 --
 -- AUTO_INCREMENT untuk tabel `post`
 --
 ALTER TABLE `post`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=19;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=32;
 
 --
 -- AUTO_INCREMENT untuk tabel `product`
 --
 ALTER TABLE `product`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=31;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=42;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
