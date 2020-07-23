33e118bb320d709b6d90eca03279bac5db1315ae
Herbeth2108 <33892612+Herbeth2108@users.noreply.github.com>
2020-07-23 16:28:24 +0700

    main page , tombol wa

--- application/views/dist/_partials/header-main.php
+++ application/views/dist/_partials/header-main.php
@@ -2,6 +2,7 @@
 <!DOCTYPE html> 
 <html lang="en">
 <head>
+
   <meta charset="UTF-8">
   <meta content="width=device-width, initial-scale=1, maximum-scale=1, shrink-to-fit=no" name="viewport">
   <title><?php echo $title; ?> &mdash; Aquawabisabi</title>
--- application/views/dist/_partials/layout-main.php
+++ application/views/dist/_partials/layout-main.php
@@ -1,16 +1,22 @@
 <?php
 defined('BASEPATH') OR exit('No direct script access allowed');
 ?>
+<div> 
+  <nav class="navbar navbar-expand-lg main-navbar sticky-top">
+    <img class="brand-img" src="<?php echo base_url('assets/img/brand6.png')?>" >
+</div>
+
 <body class="sidebar-gone">
   <div id="app">    
     <div class="main-wrapper main-wrapper-1">      
       <nav class="navbar navbar-expand-lg main-navbar sticky-top">
         <form class="form-inline mr-auto">
-          <ul class="navbar-nav mr-3">
-            <li id="toggle-sidebar"><a href="#" data-toggle="sidebar-main" class="nav-link nav-link-lg toggle-gone"><i class="fas fa-bars"></i></a></li>                                    
-          </ul>          
+          <ul class="navbar-nav mr-3">  
+            <li id="toggle-sidebar"><a href="#" data-toggle="sidebar-main" class="nav-link nav-link-lg toggle-gone"><i class="fas fa-bars"></i></a></li>                                   
+          </ul>     
+
         </form>
-        <ul class="navbar-nav navbar-center">          
+        <ul class="navbar-nav navbar-center">
           <li class="nav-item <?php if($this->uri->segment(1) == "home" || $this->uri->segment(1) == "") echo "active"; ?>"><a href="<?= base_url() ?>" class="nav-link">Home</a></li>            
           <li class="nav-item <?php if($this->uri->segment(1) == "products") echo "active"; ?>"><a href="<?= base_url('products') ?>" class="nav-link">Produk</a></li>
           <li class="nav-item <?php if($this->uri->segment(1) == "post") echo "active"; ?>"><a href="<?= base_url('post') ?>" class="nav-link">Post</a></li>
