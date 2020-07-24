e6b797cbf08cac0db64f373f80970a22b329dd66
unknown <herbeth88@gmail.com>
2020-07-24 16:49:51 +0700

    about us founder

--- application/views/pages/home.php
+++ application/views/pages/home.php
@@ -35,54 +35,83 @@ $this->load->view('dist/_partials/header-main');
         <!-- End Of Slider -->
 
         <div class="bg-white">
-          <div class="container py-5">
+          <div class="container py-5 about-main2">
             <div class="row h-100 align-items-center py-5">
-              <div class="col-lg-6">
-                <h1 class="display-5">Tentang Kita</h1>
+              <div class="col-lg-6 col-md-6">
+                <h1 class="font-bebas">Tentang Kita</h1>
                 <p class="lead mb-0 font-green">AQUA WABISABI</p>
                 <b>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
                 tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
                 quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
                 consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
-                cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
+                cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat n
                 proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</b>
-                </p>
               </div>
-              <div class="col-lg-6 d-none d-lg-block"><img  src="<?php echo base_url('assets/img/about1.jpg')?>" alt="" class="img-fluid"></div>
+                <div class="col-lg-6 col-md-6">
+                  <img class="img-fluid" src="<?php echo base_url('assets/img/about1.jpg')?>" alt="">
+                </div>
+          </div>
+        </div>
+        <div class="bg-white">
+          <div class="container about-main py-5">
+            <div class="row">
+              <div class="col-lg-6 col-md-6">
+                <h2 class="mb-0 font-green font-bebas">Visi</h2>
+                <b>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+                tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+                quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+                consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
+                cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat n
+                proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</b>                
+              </div>
+              <div class="col-lg-6 col-md-6">
+                <h2 class="mb-0 font-green font-bebas">Misi</h2>
+                <b>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+                tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+                quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+                consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
+                cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat n
+                proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</b>              
+              </div>
+            </div>
+            <div class="row">
+              <div class="col-lg-6 col-md-6">
+                <h2 class="mb-0 font-green font-bebas">Nilai Perusahaan</h2>
+                <b>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+                tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+                quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+                consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
+                cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat n
+                proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</b>                
+              </div>
+              <div class="col-lg-6 col-md-6">
+                <img  src="<?php echo base_url('assets/img/about2.jpg')?>" alt="" class="width-img">
+              </div>  
             </div>
           </div>
         </div>
 
-        <div class="bg-white py-5">
-          <div class="container py-5">
-            <div class="row align-items-center mb-5">
-              <div class="col-lg-6 order-2 order-lg-1"><i class="fa fa-bar-chart fa-2x mb-3 text-primary"></i>
-                <h2 class="font-weight-light font-green">Visi</h2>
-                <p class="font-italic text-muted mb-4"><b>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</b></p>
-              </div>
-              <div class="col-lg-5 px-5 mx-auto order-1 order-lg-2"><img class="rounded-circle about-img" src="<?php echo base_url('assets/img/about2.jpg')?>" alt="" class="img-fluid mb-4 mb-lg-0"></div>
-            </div>
-            <div class="row align-items-center">
-              <div class="col-lg-5 px-5 mx-auto font-green"><img class="rounded-circle about-img" src="<?php echo base_url('assets/img/about3.jpg')?>" alt="" class="img-fluid mb-4 mb-lg-0"></div>
-              <div class="col-lg-6">
-                <h2 class="font-weight-light font-green">Misi</h2>
-                <p class="font-italic text-muted mb-4"> <b>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. </b></p>
-              </div>
-               <div class="bg-white py-5">
-                <div class="container py-5">
-                  <div class="row align-items-center mb-5">
-                    <div class="col-lg-6 order-2 order-lg-1"><i class="fa fa-bar-chart fa-2x mb-3 text-primary"></i>
-                      <h2 class="font-weight-light font-green">Nilai Perusahaan</h2>
-                      <p class="font-italic text-muted mb-4"> <b>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. </b></p> 
-                    </div>
-                    <div class="col-lg-5 px-5 mx-auto order-1 order-lg-2"><img class="rounded-circle about-img" src="<?php echo base_url('assets/img/about4.jpg')?>"  alt="" class="img-fluid mb-4 mb-lg-0"></div>
-                  </div>
-                </div>
-              </div>
-            </div>
-          </div>
-        </div>
       <!-- End of About Us -->
+
+      <div class="bg-white">
+        <div class="row">
+          <div class="container py-5 about-main2">
+            <div class="row h-100 align-items-center py-5">
+              <div class="col-lg-6 col-md-6">
+                <h1 class="font-bebas"><u>Pendiri Aqua wabisabi</u></h1>
+                <p class="lead mb-0 font-green">Rhandy Ibrahim</p>
+                <b>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+                tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+                quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+                consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
+                cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat n
+                proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</b>
+              </div>
+                <div class="col-lg-6 col-md-6">
+                  <img class="founder-img" src="<?php echo base_url('assets/img/founder.png')?>" alt="">
+              </div>
+              </div>
+          </div>
 
         <div class="separator" style="margin-right: 15px; margin-left: 15px">
           <div class="row">
